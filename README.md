d4102a4b9c6cbb357f0ddf286c630103bb69590b
Phillip England <phillip.mark.england@gmail.com>
2024-09-02 07:01:00 -0500

    README

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ What if HTML was a programming language? With Javascript, we can *extend* HTML. 
 ## Inspiration
 Two of my favorite tools revolve around HTML's attributes. Both Tailwind and HTMX center on this idea of extracting data from attributes and doing something special with it. I believe more can be sqeezed out of this idea.
 
+
+## Attribute Listing
+
+### proxy-click
+
+#### Action
+click one element, and have it forward the click onto another element
+
+#### Use Case
+I created this to make it easy to hide a file input and click it using another, prettier, button.
+
+#### Example
+```html
+<button proxy-click='#hidden-input'>Click Me!</button>
+<input id='hidden-input' type='file' class='hidden' />
+```
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 class Htpl {
 
     constructor() {
@@ -6,19 +5,30 @@ class Htpl {
     }
 
     
-    add(attribute) {
-        this.attributes[attribute] = attribute
+    add(attribute, fn) {
+        this.attributes[attribute] = fn
     }
 
     hook() {
-        for (const key in this.attributes.keys()) {
+        for (let i = 0; i < Object.keys(this.attributes).length; i++) {
+            let key = Object.keys(this.attributes)[i]
             let elements = document.querySelectorAll(`[${key}]`)
-            for (let i = 0; i < elements.length; i++) {
-                let element = elements[i]
-                this.attributes[key](element)
+            for (let i2 = 0; i2 < elements.length; i2++) {
+                let element = elements[i2]
+                let attr = element.getAttribute(key)
+                this.attributes[key](element, attr)
             }
         }
     }
 
 
 }
+
+const htpl = new Htpl()
+
+htpl.add("proxy-click", (element, attr) => {
+    let target = document.querySelector(attr)
+    element.addEventListener('click', () => {
+        target.click()
+    })
+})
