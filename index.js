912115e5df3a6249160b910bd56a3b248c1312b9
Phillip England <phillip.mark.england@gmail.com>
2024-09-03 04:36:34 -0500

    got ht-multi-photo-form working

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ class Htpl {
 
     constructor() {
         this.attributes = {}
+        this.ctx = {}
     }
 
     
     add(attribute, fn) {
         this.attributes[attribute] = fn
+    }
+
+    stamp(element) {
+        element.setAttribute('ht-stamp', 'true')
     }
 
     hook() {
@@ -28,8 +33,6 @@ class Htpl {
         }
     }
 }
-
-
 
 const htpl = new Htpl()
 
@@ -54,4 +57,101 @@ htpl.add('ht-mass-toggle', (element, attr) => {
             }
         }
     })
+})
+
+htpl.add('ht-multi-photo-form', (element, attr) => {
+
+    function extract() {
+        let parts = attr.split(':')
+        if (parts.length < 2) {
+            console.error('ht-multi-photo-form does not have enough args')
+        }
+        let fileInputSelector = parts[0]
+        let photoContainerSelector = parts[1]
+        let canvasWrapperClasses = ""
+        if (parts[2]) {
+            canvasWrapperClasses = parts[2].split(" ")
+        }
+        let scaleRatio = 1
+        if (parts[3]) {
+            scaleRatio = Number(parts[3])
+        }
+        let fileInput = document.querySelector(fileInputSelector)
+        let photoContainer = document.querySelector(photoContainerSelector)
+        let data = new DataTransfer()
+        return {fileInput, photoContainer, scaleRatio, canvasWrapperClasses, data}
+    }
+
+    function getDiv(file, canvasWrapperClasses) {
+        let div = document.createElement('div')
+        div.setAttribute('key', file.name)
+        for (let i = 0; i < canvasWrapperClasses.length; i++) {
+            let cls = canvasWrapperClasses[i]
+            div.classList.add(cls)
+        }
+        return div
+    }
+
+    function getCanvas(scaleRatio, img) {
+        let canvas = document.createElement('canvas')
+        canvas.height = img.height * scaleRatio
+        canvas.width = img.width * scaleRatio
+        return {
+            'canvas': canvas,
+            'ctx': canvas.getContext('2d')
+        }
+    }
+
+    function draw(canvas, div, photoContainer, ctx, img) {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+        div.appendChild(canvas)
+        photoContainer.appendChild(div)
+    }
+
+    let {fileInput, photoContainer, scaleRatio, canvasWrapperClasses, data} = extract()
+
+    fileInput.addEventListener('change', (e) => {
+        e.preventDefault()
+        let file = fileInput.files[0]
+        if (!file) {
+            return
+        }
+        let reader = new FileReader()
+        if (!reader) {
+            return
+        }
+        reader.onload = function(event) {
+            let img = new Image()
+            if (!img) {
+                return
+            }
+            img.onload = function() {
+                data.items.add(file)
+                fileInput.files = data.files
+                let div = getDiv(file, canvasWrapperClasses)
+                let { canvas, ctx } = getCanvas(scaleRatio, img)
+                draw(canvas, div, photoContainer, ctx, img)
+                div.addEventListener('click', () => {
+                    let newFiles = new DataTransfer()
+                    for (let i = 0; i < fileInput.files.length; i++) {
+                        let f = fileInput.files[i]
+                        let found = false
+                        if (f.name == div.getAttribute('key') && found == false) {
+                            found = true
+                            div.remove()
+                            continue
+                        }
+                        newFiles.items.add(f)
+                    }
+                    fileInput.files = newFiles.files
+                })
+            }
+            img.src = event.target.result
+        }
+        reader.readAsDataURL(file)
+    })
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    htpl.hook()
 })
